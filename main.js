28831fcef41aa9eee31d30657e12b2ff940c265d
AdamT2529 <profileytwow1@gmail.com>
2025-09-11 18:40:19 +0000

    Adam

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Example Text Interface");
+let ti = new TextInterface(app, "Which Spongebob Character Are You?");
 let name = await ti.prompt("What is your name?");
 ti.output("Hello, " + name);
+let money = await ti.prompt("Do you believe that money buys happiness?")
+
 
