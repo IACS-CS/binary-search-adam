54c1d54d6ad96f1d5a50500efa9b3f92aa2cca3c
AdamT2529 <AdamT2529@users.noreply.github.com>
2025-09-18 19:23:55 +0000

    deploy: 2c2746c162b008906c4e9b53186ae57e0d7ccbf0

--- assets/index-5a326430.js
+++ assets/index-5a326430.js
@@ -201,17 +201,19 @@ let ti = new a(app, "Which Spongebob Character Are You?");
 let name = await ti.prompt("What is your name?");
 ti.output("Hello, " + name);
 let gender = await ti.prompt("Are you a boy or girl?");
+let isDriver;
 if (gender === "boy") {
-  await ti.prompt("Do you have a driver's license?"); 
+  isDriver = await ti.prompt("Do you have a driver's license?"); 
 } else {
-  await ti.prompt("Do you value relaxing more than working?");
+  isRelaxing = await ti.prompt("Do you value relaxing more than working?");
 }
 // If the person is a boy, we ask if they have a driver's license
 // If the person is a girl, we ask if they value relaxing more than working
-{
+if (isDriver === "no") {
+  money = await ti.prompt("Do you think that money buys happiness?"); 
+} 
+if (isDriver === "yes") {
   await ti.prompt("Do you love exercising?");
 }
-
-
-
 // If the person says no to having a drivers license, they get asked if money buys happiness
+// If the person says yes to having a drivers license, they get asked if they love exercising
